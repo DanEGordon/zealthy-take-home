055f79c91ce6ab44c523a4dfa72a25dae51ea2da
DanEGordon <triplesaxman@gmail.com>
2024-09-25 22:47:55 -0400

    removing add-user api call to see if build works

--- app/pages/pageThree/page.tsx
+++ app/pages/pageThree/page.tsx
@@ -13,36 +13,6 @@ const PageThree = () => {
   const router = useRouter();
 
   const onSubmit = async () => {
-    const email = form.userName;
-    const password = form.password;
-    const about = form.aboutMe;
-    const addressObject = form.address;
-    const address =
-      addressObject.streetAddress +
-      ", " +
-      addressObject.city +
-      " " +
-      addressObject.state +
-      ", " +
-      addressObject.zip;
-    const birthdate = form.birthdate;
-
-    if (!email) throw new Error("Email is absent");
-    if (!password) throw new Error("Password is absent");
-    if (!about) throw new Error("About Me section is absent");
-    if (!address.replaceAll(",", "")) throw new Error("Address is absent");
-    if (!birthdate) throw new Error("Birthdate is absent");
-
-    try {
-      await fetch("/api/add-user", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password, about, address, birthdate }),
-      });
-    } catch (error) {
-      console.error(error);
-    }
-
     router.push("/pages/data");
   };
 
